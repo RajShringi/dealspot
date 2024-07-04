2df4c32e938fb669bd1520c16be8dccd9f8e1d52
raj shringi <raj24shringi@gmail.com>
2024-07-04 17:07:52 +0530

    Add Slider to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Slider from "@/components/Slider";
+
 export default function Home() {
-  return <main className="h-[calc(100vh-80px)]"></main>;
+  return (
+    <main>
+      <Slider />
+    </main>
+  );
 }
