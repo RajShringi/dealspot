bcc83b731a064b19c95501105d0f7ca42bbe379c
raj shringi <raj24shringi@gmail.com>
2024-07-13 16:43:51 +0530

    add category image in fetch

--- src/sanity/sanity-utils.ts
+++ src/sanity/sanity-utils.ts
@@ -1,18 +1,16 @@
-import { createClient, groq } from "next-sanity";
+import { ProductCategory } from "@/types/ProductCategory";
+import { groq } from "next-sanity";
+import { client } from "./config/client-config";
 
-export async function getProductCategories() {
-  const client = createClient({
-    projectId: "q8ayhtjl",
-    dataset: "production",
-  });
-
+export async function getProductCategories(): Promise<ProductCategory[]> {
   const data = client.fetch(
     groq`*[_type == "productCategory"]{
         _id,
         _createdAt,
         title,
         "slug": slug.current,
-        description
+        description,
+        "Category_Image": Category_Image.asset->url
         }`
   );
   return data;
