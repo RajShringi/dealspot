f1e204e10053716bf0b0970f215b1955e4e67733
raj shringi <raj24shringi@gmail.com>
2024-07-13 16:43:13 +0530

    Add Category component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Category from "@/components/Category";
 import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
-    <main>
+    <main className="text-gray-700">
       <Slider />
+      <div>
+        <Category />
+      </div>
     </main>
   );
 }
