290c230893977e36437dfc225be0bdee03f300f9
raj shringi <raj24shringi@gmail.com>
2024-07-16 19:20:22 +0530

    create productCategoryType and productType

---
+++ src/types/ProductCategoryType.ts
@@ -0,0 +1,8 @@
+export type ProductCategoryType = {
+  _id: string;
+  _createdAt: string;
+  title: string;
+  slug: string;
+  description: string;
+  Category_Image: string;
+};
---
+++ src/types/ProductType.ts
@@ -0,0 +1,9 @@
+import { PortableTextBlock } from "next-sanity";
+
+export type ProductType = {
+  _id: string;
+  title: string;
+  slug: string;
+  Main_Image: string;
+  price: number;
+};
