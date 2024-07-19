c9ffb8e7c678563a46ee838a128c28305653ab8d
raj shringi <raj24shringi@gmail.com>
2024-07-19 16:50:36 +0530

    add query to get product details

--- src/sanity/sanity-utils.ts
+++ src/sanity/sanity-utils.ts
@@ -2,6 +2,7 @@ import { groq } from "next-sanity";
 import { client } from "./config/client-config";
 import { ProductCategoryType } from "@/types/ProductCategoryType";
 import { ProductType } from "@/types/ProductType";
+import { ProductDetailtype } from "@/types/ProductDetailType";
 
 export async function getProductCategories(): Promise<ProductCategoryType[]> {
   const data = client.fetch(
@@ -27,5 +28,25 @@ export async function getFilterProducts(slug: string): Promise<ProductType[]> {
     price,
   }
 `;
+  return client.fetch(query, { slug });
+}
+
+export async function getProductDetails(
+  slug: string
+): Promise<ProductDetailtype> {
+  const query = groq`
+   *[_type == "product" && slug.current == $slug][0]{
+    _id,
+    title,
+    "slug":slug.current,
+    "Main_Image": Main_Image.asset->url,
+    images[]{
+    "id": asset->_id,
+    "url": asset->url
+  },
+    price,
+    content
+  }
+  `;
   return client.fetch(query, { slug });
 }
